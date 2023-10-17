ebe7d16ec320d905ad1d5e89a17e984f1ef149e5
Martin Bonnin <martin@mbonnin.net>
2023-10-17 16:39:22 +0200

    initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,27 @@
 plugins {
-  id("org.jetbrains.kotlin.jvm").version("1.9.0")
+  id("org.jetbrains.kotlin.multiplatform").version("1.9.20-RC")
+}
+
+kotlin {
+  jvm()
+  macosArm64()
+  iosArm64()
+  js(IR) {
+    nodejs()
+  }
+
+  applyDefaultHierarchyTemplate {
+    group("common") {
+      group("concurrent") {
+        group("apple")
+        withJvm()
+      }
+    }
+  }
+
+  sourceSets.getByName("commonMain") {
+    dependencies {
+      implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    }
+  }
 }
