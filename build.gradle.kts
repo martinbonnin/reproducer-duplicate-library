5d72028c0e59e439fc8eaa8018a879fcef6e183d
Martin Bonnin <martin@mbonnin.net>
2023-10-17 16:42:01 +0200

    enable allWarningsAsErrors

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jetbrains.kotlin.gradle.dsl.*
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
   id("org.jetbrains.kotlin.multiplatform").version("1.9.20-RC")
 }
@@ -24,4 +27,25 @@ kotlin {
       implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     }
   }
+}
+
+fun KotlinProjectExtension.forEachCompilerOptions(block: KotlinCommonCompilerOptions.() -> Unit) {
+  when (this) {
+    is KotlinJvmProjectExtension -> compilerOptions.block()
+    is KotlinAndroidProjectExtension -> compilerOptions.block()
+    is KotlinMultiplatformExtension -> {
+      targets.all {
+        compilations.all {
+          compilerOptions.configure {
+            block()
+          }
+        }
+      }
+    }
+    else -> error("Unknown kotlin extension $this")
+  }
+}
+
+kotlin.forEachCompilerOptions {
+  allWarningsAsErrors.set(true)
 }
